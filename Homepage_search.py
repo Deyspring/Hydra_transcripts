bbcb7bee6761aa6973bfff66f304c3882c2078d7
deyspring <dey.katherine@gmail.com>
2020-01-04 20:53:50 -0800

    sucess!
    The script is activating the search after submitting a search term as hoped!

--- Homepage_search.py
+++ Homepage_search.py
@@ -7,33 +7,34 @@
 # Now I'm wondering about how the text is entered and the autocomplete java stuff. 
 # And also if the search is being blocked by some sort of automatic script sensor
 
-#I'm going to try the Firefox driver and see if that makes any difference
+#I'm going to try the Firefox driver and see if that makes any difference - nope!
+# After a bit more careful peering at the html, I noticed the onkeyup option and did 
+# research I wish I'd done sooner. It was triggering the execution of some javascript. 
+# Solution below. 
+#Fuck yeah! sucess!!! I used the click() method and pinpointed the elements using xpath
+#The element's xpaths can be copied in the developer window after selecting them. 
 
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-browser = webdriver.Chrome()
+
+browser = webdriver.Firefox()
+
 browser.get('https://research.nhgri.nih.gov/hydra/pfam/')
 
 try:
-    keywordElem = browser.find_elements_by_id('keyword')
-    print('Found element with that class name!' )
-    print(keywordElem[0].text + "keywordElem") #keyword Elem is apparently blank
-
+	searchElem = browser.find_element_by_xpath('/html/body/div[3]/form/table[4]/tbody/tr[2]/td[5]/input[1]')
+    #print('Found element with that class name!')
+   
 except:
     print('Was not able to find element with that name.')
 
-searchBoxElem = keywordElem[1]
-searchBoxElem.send_keys('ig')
+searchElem.send_keys('ig')
+#Click on the search button to activate search. 
+browser.find_element_by_xpath('/html/body/div[3]/form/table[4]/tbody/tr[2]/td[5]/input[2]').click()
+  
 
 
-searchBoxElem.send_keys(Keys.RIGHT)
-searchBoxElem.send_keys(Keys.RIGHT)
-searchBoxElem.send_keys(Keys.ENTER)
-print('pressed return')
-#searchBoxElem.submit()
-
-time.sleep(10) # This allows me a good amount of time to look at the results before closing browser. 
-browser.close() # If I don't close the browser, they pile up
-quit()
+#time.sleep(10) # This allows me a good amount of time to look at the results before closing browser. 
+#browser.close() # If I don't close the browser, they pile up
+#quit()
