2ab15b72dcab1c99f377d8e2554be6e2ca44255e
deyspring <dey.katherine@gmail.com>
2020-01-04 16:13:15 -0800

    added browser closer
    I added a browser closer command and added some selenium webscraping templates to the repo for reference.

--- Homepage_search.py
+++ Homepage_search.py
@@ -3,13 +3,13 @@
 # and then identify a resulting link
 
 #This code is pretty simple right now, but after the text is entered and submitted, no search items return 
-#It's pretty clear the search did happen, but I'm wondering if it was too quick. 
-# It does not appear to be a quickness thing
+# Something happens, the browser refreshes, but nothing else. 
 # Now I'm wondering about how the text is entered and the autocomplete java stuff. 
-#And also if the search is being blocked by some sort of automatic script sensor
+# And also if the search is being blocked by some sort of automatic script sensor
+
+#I'm going to try the Firefox driver and see if that makes any difference
 
 import time
-#time.sleep()
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -19,6 +19,8 @@ browser.get('https://research.nhgri.nih.gov/hydra/pfam/')
 try:
     keywordElem = browser.find_elements_by_id('keyword')
     print('Found element with that class name!' )
+    print(keywordElem[0].text + "keywordElem") #keyword Elem is apparently blank
+
 except:
     print('Was not able to find element with that name.')
 
@@ -32,14 +34,6 @@ searchBoxElem.send_keys(Keys.ENTER)
 print('pressed return')
 #searchBoxElem.submit()
 
-
-
-"""
-try:
-    submitElem = browser.find_elements_by_name('search')
-    print('Found element search with that class name!' )
-except:
-    print('Was not able to find element with that name.')
-
-submitElem.submit()"""
-
+time.sleep(10) # This allows me a good amount of time to look at the results before closing browser. 
+browser.close() # If I don't close the browser, they pile up
+quit()
